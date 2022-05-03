791b00c842dee6227518ab11b1e095eb4a113483
Dayan Ruben <mail@dayanruben.com>
2022-05-03 12:14:59 -0300

    Migrated Android Gradle build file to Multiplatform structure, and updated some dependencies

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application")
-    kotlin("android")
+    kotlin("multiplatform")
 }
 
 android {
@@ -17,11 +17,14 @@ android {
             isMinifyEnabled = false
         }
     }
-}
 
-dependencies {
-    implementation(project(":shared"))
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+    dependencies {
+        implementation(project(":shared"))
+        implementation("com.google.android.material:material:1.5.0")
+        implementation("androidx.appcompat:appcompat:1.4.1")
+        implementation("androidx.constraintlayout:constraintlayout:2.1.3")
+    }
+}
+kotlin {
+    android()
 }
