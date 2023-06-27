5251345b6721aa80a94856ade3855a4010bb1af7
pahill <pamela.a.hill@gmail.com>
2023-06-27 10:30:54 +0200

    Kostya feedback

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -6,7 +6,13 @@ plugins {
 kotlin {
     targetHierarchy.default()
 
-    android()
+    android() {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
 
     listOf(
         iosX64(),
@@ -19,28 +25,18 @@ kotlin {
     }
 
     sourceSets {
-        val commonMain by getting
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test-common"))
                 implementation(kotlin("test-annotations-common"))
             }
         }
-        val androidMain by getting
         val androidUnitTest by getting {
             dependencies {
                 implementation(kotlin("test-junit"))
                 implementation("junit:junit:4.13.2")
             }
         }
-        val iosX64Main by getting
-        val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
-        val iosMain by getting
-        val iosX64Test by getting
-        val iosArm64Test by getting
-        val iosSimulatorArm64Test by getting
-        val iosTest by getting
     }
 }
 
