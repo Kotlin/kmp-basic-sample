a8395d772a169576ca0583ad2e0a91c7dd4e2c6a
Rizwanul Haque <riztech.dev@gmail.com>
2023-10-04 12:36:49 +0530

    Fix for build issue on Xcode 15

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.21")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
         classpath("com.android.tools.build:gradle:8.0.2")
     }
 }
